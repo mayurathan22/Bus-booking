7f5509da9322ea468935a535b1cdc337fff6c3b3
mayurathan22 <thavaranjanmayurathan95@gmail.com>
2021-08-20 01:32:53 +0530

    web_route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use App\Http\Controllers\BusController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\TripController;
+use App\Http\Controllers\TicketBookingController;
+
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 /*
@@ -27,9 +30,14 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 // Auth::routes();
 
-Route::get('/admin/bus', [App\Http\Controllers\HomeController::class, 'adminIndex'])->name('admin-home');
+Route::get('/admin/bus.', 'App\Http\Controllers\HomeController@adminIndex')->name('admin-homeIndex');
 
-// Route::get('/user/bus', [App\Http\Controllers\HomeController::class, 'userDashboard'])->name('user-home');
+Route::get('admin/bus/delete/{id}',[BusController::class, 'destroy']);
+
+Route::get('user/bus', [App\Http\Controllers\BusController::class, 'userBusIndex'])->name('user-bus');
+Route::get('/user/dashboard', [App\Http\Controllers\DashboardController::class, 'index'])->name('user-dashboard');
+
+// Route::get('/user/bus','App\Http\Controllers\BusController@userBusIndex')->name('user-getBuses');
 
 
 // Login, Register
@@ -37,34 +45,15 @@ Route::get('/', function() {
     return view('auth.login');
 });
 
-// Route::get('/register', function() {
-//     // return view('auth.register');
-// });
 
-// Admin Routes
-Route::get('/admin/trip', function() {
-    return view('admin.trip');
-})->name('admin-trip');
-
-// Route::get('/admin/bus', function() {
-//     return view('admin.bus');
-// })->name('admin-bus');
-
+Route::get('/admin/bus', [App\Http\Controllers\BusController::class, 'Index'])->name('adminbus-index');
 Route::post('/admin/bus', [BusController::class,'store'])->name('admin-bus');
-Route::post('/admin/trip', [TripController::class,'store'])->name('admin-bustrip');
-// Route::post('/admin/trip', [TripController::class,'create'])->name('admin-bustripCreate');
 
 
-// Route::get('/admin/trip', [TripController::class,'getBuses'])->name('admin-getTripbus');
-// Route::get('/admin/trip', [BusController::class,'index']);
-// Route::get('/admin/trip', [BusController::class,'create']);
+Route::post('/admin/trip', [TripController::class,'store'])->name('admin-trip');
+Route::get('/admin/trip', [App\Http\Controllers\TripController::class, 'Index'])->name('admintrip-index');
+Route::get('admin/trip/delete/{id}',[TripController::class, 'destroy']);
 
-// Route::get('/admin/test', 'App\Http\Controllers\BusController@index');
-
-
-// Route::post('/user/bus', [BusController::class,'store'])->name('user-bus');
-
-// Route::namespace('App\Http\Controllers')->group(function () { Route::resource('admintrip', 'TripController');});
 
 
 Route::get('/admin/route', function() {
@@ -73,17 +62,21 @@ Route::get('/admin/route', function() {
 
 
 // user Routes
-Route::get('/user/dashboard', function() {
-    return view('user.dashboard');
-})->name('user-dashboard');
+// Route::get('/user/dashboard', function() {
+//     return view('user.dashboard');
+// })->name('user-dashboard');
 
-Route::get('/user/bus', function() {
-    return view('user.bus');
-})->name('user-bus');
+// Route::get('/user/bus', function() {
+//     return view('user.bus');
+// })->name('user-bus');
 
-Route::get('/user/bus/book', function() {
-    return view('user.bus-book');
-})->name('user-bus-book');
+// Route::get('/user/bus/book', function() {
+//     return view('user.bus-book');
+// })->name('user-bus-book');
+
+Route::get('user/bus/{id}/book', [TicketBookingController::class, 'Index'])->name('user-bus-book');
+Route::post('user/bus/{id}/book', [TicketBookingController::class, 'store'])->name('user-bus-book-store');
+Route::get('user/dashboard/delete/{id}',[DashboardController::class, 'destroy']);
 
 
 
